ef5f6a1c90a15f8ce77d2b8abd41aae292b7ce68
akridaus277 <fachryfirdaus277@yahoo.com>
2022-10-03 09:17:35 +0700

    summary

---
+++ Sesi_4/latihan2.java
@@ -0,0 +1,13 @@
+package Sesi_4;
+
+public class latihan2 {
+    public static void main(String[] args) {
+        // Buat Variabel
+        int a = 12;
+        int b = 8;
+        int c = 5;
+        // Hitung
+        int hasil1 = a + b - c;
+        
+    }
+}
